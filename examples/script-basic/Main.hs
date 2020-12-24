1c415e3a4bf19ef61990a0acc444cd36a285f429
Adam Flott <adam@adamflott.com>
2020-12-24 12:17:11 -0500

    Correct example name

--- examples/script-basic/Main.hs
+++ examples/script-basic/Main.hs
@@ -26,7 +26,7 @@ myAppMain _exec _run_ctx _opts _st = do
 --------------------------------------------------------------------------------
 
 app :: (AllAppConstraints m) => AppSpec m e NoEvent NoEnv NoOpts NoConfig ()
-app = defaultAppSpec { appName = "amf-script", appSetup = noAppSetup, appMain = myAppMain, appEnd = noAppFinish }
+app = defaultAppSpec { appName = "amf-script-basic", appSetup = noAppSetup, appMain = myAppMain, appEnd = noAppFinish }
 
 main :: IO ()
 main = runAppSpecAsScript app
